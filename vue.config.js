6959ccca8df80839c9f03de8616ab4b9179f5cae
KSJ <kimseungjae@gmail.com>
2023-02-13 17:45:14 +0900

    init 설정

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+//prefetch 삭제
+module.exports = {
+  chainWebpack: config => {
+    config.plugins.delete('prefetch');
+  }
+}
